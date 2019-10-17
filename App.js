810770ec92c639af019cd6bff04f29a5565a9985
silvajunior <jros2.silva@gmail.com>
2019-10-17 15:51:12 -0300

    Criando o MainStack

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {Text} from 'react-native';
+
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/es/integration/react';
 import {store, persistor} from './src/store';
 
+import MainStack from './src/navigators/MainStack';
+
 //importar STACK
+// - Preload
+// - StarterStack
+// - AppTab
 
 export default () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Text>...</Text>
+      <MainStack />
     </PersistGate>
   </Provider>
 );
